61b62fc8c1dab77e052cf95b217045e299855e80
Tkkytrs <120646466+Tkkytrs@users.noreply.github.com>
2025-02-15 19:16:30 +0530

    Upload main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+os.system("pip install mnemonic")
+os.system("pip install base58")
+os.system("pip install bs4 colorama requests")
+os.system("pip install bech32")
+os.system("pip install hmac")
+os.system("pip install struct")
+os.system("pip install ecdsa")
+print(" The Installing Is Complete, Please Run 'goood.py' to run the main code, \neg:\npython goood.py\npython3 goood.py\npy goood.py")
